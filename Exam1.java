0545d02e07a79e2e3c8dd280370c7d99b9124db3
rudalsdl0822 <89821065+rudalsdl0822@users.noreply.github.com>
2021-09-10 11:53:18 +0900

    210910 Exam1Edit

--- Exam1.java
+++ Exam1.java
@@ -3,12 +3,11 @@ import java.util.Scanner;
 public class Exam1 {
 
 	public static void main(String[] args) {
-		double avgScores[] = { 85.6, 79.5, 83.1, 80.0, 78.2, 75.0 };
+		double avgScores[] = { 85.6, 79.5, 83.1, 80.0, 78.2, 75.0, 80.0, 90.0, 70.0, 60.0};
 		Scanner sc = new Scanner(System.in);
 		int num1 = sc.nextInt() - 1;
 		int num2 = sc.nextInt() - 1;
 		sc.close();
 		System.out.printf("%.1f", avgScores[num1] + avgScores[num2]);
 	}
-
 }
